e8e4c46215d0c71b39f42827742f5a84ff68e773
wei.sun <wei.sun@spreadtrum.com>
2014-07-14 13:10:54 +0800

    nil

---
+++ bin/pinyin.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+require 'sqlite3'
+require 'iconv'
+
+conv = Iconv.new('UTF-8', 'GBK')
+
+$db = SQLite3::Database.new('pinyin.db')
+
+def get_pinyin (name)
+  ret = ""
+  name.each_char {|v|
+    sql = "select pinyin from pinyin where char = \"#{v}\""
+    rows = $db.execute(sql)
+    if rows
+      ret << rows[0][0]
+    end
+  }
+  return ret
+end
+
+def get_fuxing (name)
+  sql = "select pinyin from fuxing where char = \"#{name}\""
+  rows = $db.execute(sql)
+  if rows.empty?
+    return nil
+  else
+    return rows[0][0]
+  end  
+end
+
+surname_py = ""
+name_py = ""
+
+name = conv.conv(ARGV[0])
+surname_py = get_fuxing(name[0,2])
+if surname_py
+  name_py = get_pinyin(name[2,name.length])
+else
+  surname_py = get_pinyin(name[0])
+  name_py = get_pinyin(name[1,name.length])
+end
+
+puts surname_py+" "+name_py
+    
+  
