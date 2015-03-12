4d336c3c8b685b33e280cf906df0f74776a0f11e
sunway <sunwayforever@gmail.com>
2015-03-12 21:20:16 +0800

    xmpp4r is buggy,  change to sleekxmpp

--- bin/xmpp.py
+++ bin/xmpp.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
-import logging
 import sys
-
 from sleekxmpp import ClientXMPP
 from sleekxmpp.exceptions import IqError, IqTimeout
 
-
-class EchoBot(ClientXMPP):
+class Bot(ClientXMPP):
 
     def __init__(self, jid, password):
         ClientXMPP.__init__(self, jid, password)
@@ -22,21 +19,12 @@ class EchoBot(ClientXMPP):
         if msg['type'] in ('chat', 'normal'):
 	    body = msg['body'].decode("GBK").encode("UTF-8")
 	    print ("xmpp message: %s:%s\n" % (msg['from'],body))
-       #     msg.reply("Thanks for sending\n%(body)s" % msg).send()
 
 if __name__ == '__main__':
-    # Ideally use optparse or argparse to get JID,
-    # password, and log level.
-
-    #logging.basicConfig(level=logging.DEBUG,
-                        #format='%(levelname)-8s %(message)s')
-
-    xmpp = EchoBot(sys.argv[1]+'@localhost/xkx', '123456')
+    xmpp = Bot(sys.argv[1]+'@localhost/xkx', '123456')
     xmpp.connect(("45.62.101.109",5222))
     xmpp.process(block=False)
     while (True):
 	line = sys.stdin.readline().decode("GBK").encode("UTF-8")
-	xmpp.send_message(mto="messenger@localhost/xkx",
-                          mbody=line,
-                          mtype='chat')
+	xmpp.send_message(mto="messenger@localhost/xkx", mbody=line, mtype='chat')
 	
