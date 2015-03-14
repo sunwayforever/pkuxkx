a8a32d0cf9f4ae0ba21613e7a80c960a9595976e
sunway <sunwayforever@gmail.com>
2015-03-14 09:19:51 +0800

    nil

--- bin/xmpp.py
+++ bin/xmpp.py
@@ -4,10 +4,8 @@ from sleekxmpp import ClientXMPP
 from sleekxmpp.exceptions import IqError, IqTimeout
 
 class Bot(ClientXMPP):
-
     def __init__(self, jid, password):
         ClientXMPP.__init__(self, jid, password)
-
         self.add_event_handler("session_start", self.session_start)
         self.add_event_handler("message", self.message)
 
@@ -17,7 +15,7 @@ class Bot(ClientXMPP):
 
     def message(self, msg):
         if msg['type'] in ('chat', 'normal'):
-	    print ("xmpp message: %s:%s\n" % (msg['from'],msg['body']))
+            print ("xmpp message: %s:%s\n" % (msg['from'],msg['body']))
 
 if __name__ == '__main__':
     xmpp = Bot(sys.argv[1]+'@localhost/xkx', '123456')
@@ -26,4 +24,4 @@ if __name__ == '__main__':
     while (True):
         line = sys.stdin.readline()
         xmpp.send_message(mto="messenger@localhost/xkx", mbody=line, mtype='chat')
-	
+
