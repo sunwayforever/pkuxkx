cf2956f00f463f91837d0598b7ef9f58fb39b6c5
wei.sun <wei.sun@spreadtrum.com>
2015-03-26 18:32:53 +0800

    支持指定地点的遍历

--- bin/gps/traverse.py
+++ bin/gps/traverse.py
@@ -9,31 +9,39 @@ from .common import open_database
 from .gps import shortest_path_no_weight
 from ..tintin import Tintin
 
-def traverse(conn, roomno):
+def traverse(conn, roomno, location = None):
     traverse_path = []
-    stack = [roomno]
+    stack = [(roomno,"NULL")]
     visited = set()
     last_room_no = roomno
     
     while len(stack) != 0:
-        dst_room_no = stack.pop()
-        if last_room_no != dst_room_no:
-            traverse_path.extend(shortest_path_no_weight(conn, last_room_no, dst_room_no))
+        (dst_room_no,dst_room_name) = stack.pop()
 
-        last_room_no = dst_room_no
-        visited.add(last_room_no)
+        if location is None or dst_room_name == location:
+            if last_room_no != dst_room_no:
+                traverse_path.extend(shortest_path_no_weight(conn, last_room_no, dst_room_no))
+
+            last_room_no = dst_room_no
         
-        sql = "select dst_room_no from room_and_entrance where src_room_no = %d and src_room_zone == dst_room_zone" % (last_room_no)
+        visited.add(dst_room_no)
+        
+        sql = "select dst_room_no, dst_room_name from room_and_entrance where src_room_no = %d and src_room_zone == dst_room_zone" % (dst_room_no)
         for row in conn.execute(sql).fetchall():
             if row[0] not in visited:
-                stack.append(row[0])
+                stack.append((row[0],row[1]))
 
     traverse_path.extend(shortest_path_no_weight(conn, last_room_no, roomno))
     return traverse_path
     
 if __name__ == "__main__":
     conn = open_database()
-    traverse_path = traverse(conn, int(sys.argv[1]))
+
+    if len(sys.argv) == 3:
+        traverse_path = traverse(conn, int(sys.argv[1]), sys.argv[2])
+    else:
+        traverse_path = traverse(conn, int(sys.argv[1]))
+        
     tt = Tintin()
     tt.write("#list {gps_path} create {%s}" % (";".join(traverse_path)))
     
