bee2d8170325941761231565874c1ef7df176d93
sunway <sunwayforever@gmail.com>
2015-03-28 14:01:17 +0800

    nil

--- bin/gps/check_room.py
+++ bin/gps/check_room.py
@@ -6,20 +6,32 @@ import sys
 from .common import open_database
 from ..tintin import Tintin
 
-def detect_room (conn, zone, room, desc, exits):
+def check_room (conn, zone, room, desc, exits):
+    sql = "select roomno from mud_room where roomname = '%s'" % (room)
+    rows = conn.execute(sql).fetchall();
+    if len(rows) == 1:
+        return rows[0][0]
+    
+    sql = "select roomno from mud_room where roomname = '%s' and description = '%s'" % (room, desc)
+    rows = conn.execute(sql).fetchall();
+    if len(rows) == 1:
+        return rows[0][0]
+    
     sql = "select roomno from mud_room where roomname = '%s' and description = '%s' and exits = '%s'" % (room, desc, exits)
     rows = conn.execute(sql).fetchall();
     if len(rows) == 1:
         return rows[0][0]
+    
     sql = "select roomno from mud_room where zone = '%s' and roomname = '%s' and description = '%s' and exits = '%s'" % (zone, room, desc, exits)
-    row = conn.execute(sql).fetchone()
-    if not row:
-        return -1;
-    return row[0];
+    rows = conn.execute(sql).fetchall();
+    if len(rows) == 1:
+        return rows[0][0]
+
+    return -1;
 
 if __name__ == "__main__":
     conn = open_database()
-    roomno = detect_room (conn, sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    roomno = check_room (conn, sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
     tt = Tintin()
     tt.write ("#var gps.roomno %d;" % (roomno))
 
