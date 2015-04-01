8ea33446507f1c42d5fa5b552c269e8b9286839c
wei.sun <wei.sun@spreadtrum.com>
2015-04-01 18:41:38 +0800

    nil

--- bin/gps/traverse.py
+++ bin/gps/traverse.py
@@ -8,12 +8,19 @@ import traceback
 from .common import open_database
 from .get_path import shortest_path_no_weight
 from ..tintin import Tintin
+from ..util import logger
 
 def traverse(conn, roomno, location = None):
     traverse_path = []
     stack = [(roomno,"NULL")]
     visited = set()
     last_room_no = roomno
+
+    if location is not None:
+        sql = "select distinct(dst_room_zone) from room_and_entrance where src_room_zone = \
+        (select zone from mud_room where roomno = %d) and direction not glob '*[^A-z]*'" % (roomno)
+
+        zones = ",".join(["'%s'" % (row[0]) for row in conn.execute(sql).fetchall()])
     
     while len(stack) != 0:
         (dst_room_no,dst_room_name) = stack.pop()
@@ -25,8 +32,12 @@ def traverse(conn, roomno, location = None):
             last_room_no = dst_room_no
         
         visited.add(dst_room_no)
-        
-        sql = "select dst_room_no, dst_room_name from room_and_entrance where src_room_no = %d and src_room_zone == dst_room_zone" % (dst_room_no)
+
+        if location is not None:
+            sql = "select dst_room_no, dst_room_name from room_and_entrance where src_room_no = %d and dst_room_zone in (%s)" % (dst_room_no, zones)
+        else:
+            sql = "select dst_room_no, dst_room_name from room_and_entrance where src_room_no = %d and src_room_zone = dst_room_zone" % (dst_room_no)
+
         for row in conn.execute(sql).fetchall():
             if row[0] not in visited:
                 stack.append((row[0],row[1]))
