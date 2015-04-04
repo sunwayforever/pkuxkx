e9b12b92545a58a1f839871358fc41366a74e021
sunway <sunwayforever@gmail.com>
2015-04-04 18:26:00 +0800

    nil

--- bin/gps/common.py
+++ bin/gps/common.py
@@ -5,9 +5,5 @@ import os
 import sqlite3
 
 def open_database():
-    if os.path.isfile("db/gps/rooms.db"):
-        conn = sqlite3.connect("db/gps/rooms.db")
-    else:
-        conn = sqlite3.connect("db/gps_sample/rooms.db")
-    return conn
+    return sqlite3.connect("db/rooms.db")
     
