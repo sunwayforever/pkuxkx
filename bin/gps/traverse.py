b5add8e8b8ee2eae91e5a381734b935647bf145f
sunway <sunwayforever@gmail.com>
2015-05-09 10:50:26 +0800

    nil

--- bin/gps/traverse.py
+++ bin/gps/traverse.py
@@ -10,12 +10,16 @@ from .get_path import get_path_unchecked
 from ..common import Tintin
 from ..common import logger
 
-def traverse_bfs(conn, roomno):
+def traverse_bfs(conn, roomno, location=None):
     traverse_path = []
     stack = [(roomno,"NULL")]
     visited = set()
     last_room_no = roomno
-    bfs_max_count = 8
+    if location == None:
+        bfs_max_count = 10
+    else:
+        bfs_max_count = 50
+        
     while len(stack) != 0:
         bfs_max_count = bfs_max_count-1
         if bfs_max_count<0:
@@ -23,9 +27,10 @@ def traverse_bfs(conn, roomno):
         
         (dst_room_no,dst_room_name) = stack.pop(0)
 
-        if last_room_no != dst_room_no:
-            traverse_path.extend(get_path_unchecked(conn, last_room_no, dst_room_no,"1,-1,1,-1,-1,1,1"))
-            last_room_no = dst_room_no
+        if location == None or dst_room_name == location:
+            if last_room_no != dst_room_no:
+                traverse_path.extend(get_path_unchecked(conn, last_room_no, dst_room_no,"1,-1,1,-1,-1,1,1"))
+                last_room_no = dst_room_no
             
         visited.add(dst_room_no)
 
@@ -38,56 +43,33 @@ def traverse_bfs(conn, roomno):
     traverse_path.extend(get_path_unchecked(conn, last_room_no, roomno,"1,-1,1,-1,-1,1,1"))
     return traverse_path
 
-def traverse_dfs(conn, roomno):
+def traverse_dfs(conn, roomno, location=None):
     traverse_path = []
     stack = [(roomno,"NULL")]
     visited = set()
     last_room_no = roomno
-        
+
+    # sql = "select distinct(dst_room_zone) from room_and_entrance where src_room_zone = \
+    # (select zone from mud_room where roomno = %d) and is_boundary = 0" % (roomno)
+    
+    # zones = ",".join(["'%s'" % (row[0]) for row in conn.execute(sql).fetchall()])
+
     while len(stack) != 0:
         (dst_room_no,dst_room_name) = stack.pop()
 
-        if last_room_no != dst_room_no:
-            traverse_path.extend(get_path_unchecked(conn, last_room_no, dst_room_no,"1,-1,1,-1,-1,1,1"))
-            last_room_no = dst_room_no
+        if location == None or dst_room_name == location:
+            if last_room_no != dst_room_no:
+                traverse_path.extend(get_path_unchecked(conn, last_room_no, dst_room_no,"1,-1,1,-1,-1,1,1"))
+                last_room_no = dst_room_no
             
         visited.add(dst_room_no)
 
         sql = "select dst_room_no, dst_room_name from room_and_entrance where src_room_no = %d and src_room_zone = dst_room_zone \
         and is_boundary = 0" % (dst_room_no)
 
-        for row in conn.execute(sql).fetchall():
-            if row[0] not in visited:
-                stack.append((row[0],row[1]))
-
-    traverse_path.extend(get_path_unchecked(conn, last_room_no, roomno,"1,-1,1,-1,-1,1,1"))
-    return traverse_path
-
-
-def traverse_location(conn, roomno, location):
-    traverse_path = []
-    stack = [(roomno,"NULL")]
-    visited = set()
-    last_room_no = roomno
-
-    sql = "select distinct(dst_room_zone) from room_and_entrance where src_room_zone = \
-    (select zone from mud_room where roomno = %d) and is_boundary = 0" % (roomno)
-
-    zones = ",".join(["'%s'" % (row[0]) for row in conn.execute(sql).fetchall()])
+        # sql = "select dst_room_no, dst_room_name from room_and_entrance where src_room_no = %d and dst_room_zone in (%s) \
+        # and is_boundary = 0" % (dst_room_no, zones)
         
-    while len(stack) != 0:
-        (dst_room_no,dst_room_name) = stack.pop()
-
-        if dst_room_name == location:
-            if last_room_no != dst_room_no:
-                traverse_path.extend(get_path_unchecked(conn, last_room_no, dst_room_no,"1,-1,1,-1,-1,1,1"))
-            last_room_no = dst_room_no
-            
-        visited.add(dst_room_no)
-
-        sql = "select dst_room_no, dst_room_name from room_and_entrance where src_room_no = %d and dst_room_zone in (%s) \
-        and is_boundary = 0" % (dst_room_no, zones)
-
         for row in conn.execute(sql).fetchall():
             if row[0] not in visited:
                 stack.append((row[0],row[1]))
@@ -97,12 +79,14 @@ def traverse_location(conn, roomno, location):
     
 if __name__ == "__main__":
     conn = open_database()
-
+    roomno = int(sys.argv[1])
     if len(sys.argv) == 3:
-        traverse_path = traverse_location(conn, int(sys.argv[1]), sys.argv[2])
+        location = sys.argv[2]
     else:
-        traverse_path = traverse_bfs(conn, int(sys.argv[1]))
-        traverse_path.extend(traverse_dfs(conn,int(sys.argv[1])))
+        location = None
+
+    traverse_path = traverse_bfs(conn, roomno, location)
+    traverse_path.extend(traverse_dfs(conn,roomno, location))
         
     tt = Tintin()
     tt.write("#list {gps_path} create {%s}" % (";".join(traverse_path)))
