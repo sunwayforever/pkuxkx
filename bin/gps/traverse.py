e3e696bc1a6ac2de96c9b43cb30e3038cbd4a7c4
wei.sun <wei.sun@spreadtrum.com>
2015-05-16 08:32:53 +0800

    nil

--- bin/gps/traverse.py
+++ bin/gps/traverse.py
@@ -18,7 +18,7 @@ def traverse_bfs(conn, roomno, location=None):
     if location == None:
         bfs_max_count = 10
     else:
-        bfs_max_count = 50
+        bfs_max_count = 10
         
     while len(stack) != 0:
         bfs_max_count = bfs_max_count-1
