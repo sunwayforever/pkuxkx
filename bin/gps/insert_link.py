4777953b5f326c4a453aec811f9814e6c1ed2f7e
wei.sun <wei.sun@spreadtrum.com>
2015-05-26 16:48:12 +0800

    nil

--- bin/gps/insert_link.py
+++ bin/gps/insert_link.py
@@ -6,10 +6,10 @@ import sys
 from .common import open_database
 
 def insert_link(conn, src_room_no, dst_room_no, direction, reversed_direction):
-    if direction != "":
+    if direction != "nil":
         sql = "insert or replace into mud_entrance values (%d, %d, '%s', 0, 0)" % (src_room_no, dst_room_no, direction.replace(",",";"))
         conn.execute(sql)
-    if reversed_direction != "":
+    if reversed_direction != "nil":
         sql = "insert or replace into mud_entrance values (%d, %d, '%s', 0, 0)" % (dst_room_no, src_room_no, reversed_direction.replace(",",";"))
         conn.execute(sql)
     conn.commit()
--- bin/gps/traverse.py
+++ bin/gps/traverse.py
@@ -18,7 +18,7 @@ def traverse_bfs(conn, roomno, location=None):
     if location == None:
         bfs_max_count = 10
     else:
-        bfs_max_count = 3
+        bfs_max_count = 50
         
     while len(stack) != 0:
         bfs_max_count = bfs_max_count-1
