f736b851a15a50bc646486ef5306a9e07715415c
sunway <sunwayforever@gmail.com>
2015-07-25 11:51:19 +0800

    nil

--- bin/gps/get_area.py
+++ bin/gps/get_area.py
@@ -11,14 +11,9 @@ def get_area(conn, desc):
     desc = fixup_area(desc)
     sql = "select distinct(zone) from mud_room";
     rows = conn.execute(sql).fetchall()
-    actual_zone=""
     for row in rows:
-        prefix=os.path.commonprefix((desc, row[0]))
-        if len(prefix) != 0:
-            current_zone=row[0]
-            if len(current_zone) > len(actual_zone):
-                actual_zone = current_zone
-    return actual_zone;
+        if desc.startswith (row[0]):
+            return row[0]
 
 if __name__ == "__main__":
     conn = open_database()
