a61420d13818746e26773eb35994912a5786a1e7
wei.sun <wei.sun@spreadtrum.com>
2015-10-13 17:53:56 +0800

    nil

--- bin/xmpp.py
+++ bin/xmpp.py
@@ -3,6 +3,7 @@ import sys
 import re
 from sleekxmpp import ClientXMPP
 from sleekxmpp.exceptions import IqError, IqTimeout
+from urllib.request import urlopen
 
 class Bot(ClientXMPP):
     def __init__(self, jid, password):
@@ -18,6 +19,13 @@ class Bot(ClientXMPP):
         if msg['type'] in ('chat', 'normal'):
             print ("xmpp message: %s:%s\n" % (msg['from'],msg['body']))
 
+def send_image(xmpp, url):
+    html = urlopen(url).readline().decode('utf-8')
+    m = re.match(".*img src=\"(.*)\" alt",html)
+    if m:
+        image_url = "http://pkuxkx.net/antirobot/"+m.group(1)
+        xmpp.send_message(mto="messenger@v587.info/xkx", mbody=image_url, mtype='chat')
+        
 if __name__ == '__main__':
     xmpp = Bot(sys.argv[1]+'@v587.info/xkx', '123456')
     xmpp.connect(("v587.info",5222))
@@ -30,4 +38,7 @@ if __name__ == '__main__':
             xmpp.send_presence(pstatus=status, pshow='xa')
         else:
             xmpp.send_message(mto="messenger@v587.info/xkx", mbody=line, mtype='chat')
+            m = re.match("http://pkuxkx.net/antirobot/robot.php", line)
+            if m:
+                send_image(xmpp, line)
 
