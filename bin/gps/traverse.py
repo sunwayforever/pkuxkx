34c9aba1a2049573b50a767656c295ab4ded98d4
wei.sun <wei.sun@spreadtrum.com>
2015-11-22 15:02:57 +0800

    nil

--- bin/gps/traverse.py
+++ bin/gps/traverse.py
@@ -11,6 +11,26 @@ from ..common import Tintin
 from ..common import logger
 
 TRAVERSE_WEIGHT = "1,-1,1,-1,-1,1,1,1"
+def get_neighbors(mud, roomno):
+    stack = [roomno,-1]
+    visited = set()
+    bfs_max_count = 5
+
+    while len(stack) != 0:
+        if bfs_max_count<0:
+            break
+        dst_room_no = stack.pop(0)
+        if dst_room_no == -1:
+            stack.append(-1)
+            bfs_max_count = bfs_max_count-1
+            continue
+        visited.add(dst_room_no)
+        for link in mud.neighbours[dst_room_no]:
+            if link.is_boundary == 0 and link.linkroomno not in visited:
+                stack.append(link.linkroomno)
+            
+    return visited
+
 def traverse_bfs(mud, roomno, location):
     traverse_path = []
     stack = [(roomno,"NULL")]
@@ -19,7 +39,7 @@ def traverse_bfs(mud, roomno, location):
     if location == "nil":
         bfs_max_count = 10
     else:
-        bfs_max_count = 100
+        bfs_max_count = 50
         
     while len(stack) != 0:
         bfs_max_count = bfs_max_count-1
@@ -42,12 +62,11 @@ def traverse_bfs(mud, roomno, location):
     traverse_path.extend(mud.get_path(last_room_no, roomno))
     return traverse_path
 
-def traverse_dfs(mud, roomno, location):
+def traverse_dfs(mud, roomno, location, candidate=None):
     traverse_path = []
     stack = [(roomno,"NULL")]
     visited = set()
     last_room_no = roomno
-
     while len(stack) != 0:
         (dst_room_no,dst_room_name) = stack.pop()
 
@@ -57,10 +76,17 @@ def traverse_dfs(mud, roomno, location):
                 last_room_no = dst_room_no
             
         visited.add(dst_room_no)
-
+        
         for link in mud.neighbours[dst_room_no]:
-            if link.is_boundary == 0 and link.src_room_zone == link.dst_room_zone and link.linkroomno not in visited:
-                stack.append((link.linkroomno,link.linkroomname))
+            if link.is_boundary == 1 or link.linkroomno in visited:
+                continue
+            
+            if candidate is not None:
+                if link.linkroomno in candidate:
+                    stack.append((link.linkroomno,link.linkroomname))
+            else:
+                if link.src_room_zone == link.dst_room_zone:
+                    stack.append((link.linkroomno,link.linkroomname))    
 
     traverse_path.extend(mud.get_path(last_room_no, roomno))
     return traverse_path
@@ -69,11 +95,16 @@ if __name__ == "__main__":
     conn = open_database()
     roomno = int(sys.argv[1])
     location = sys.argv[2]
-    minor = sys.argv[3]
+    mode = sys.argv[3]
 
     mud = MudRoom(conn, TRAVERSE_WEIGHT)
-    traverse_path = traverse_bfs(mud, roomno, location)
-    if (minor == "nil"):
+    if mode == "bfs":
+        traverse_path = traverse_bfs(mud, roomno, location)
+    elif mode == "dfs":
+        candidate = get_neighbors(mud,roomno)
+        traverse_path = traverse_dfs(mud, roomno, location, candidate)
+    else:
+        traverse_path = traverse_bfs(mud, roomno, location)
         traverse_path.extend(traverse_dfs(mud, roomno, location))
         
     tt = Tintin()
