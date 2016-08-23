cfee3e08781c25b944e46351f4e46b6d75d98b67
wei.sun <wei.sun@spreadtrum.com>
2016-08-23 14:37:00 +0800

    nil

--- bin/xmpp.py
+++ bin/xmpp.py
@@ -12,7 +12,7 @@ import socket
 
 class Bot(ClientXMPP):
     def __init__(self, jid):
-        ClientXMPP.__init__(self, jid+'@v587.info/xkx', "123456")
+        ClientXMPP.__init__(self, jid+'@localhost/xkx', "123456")
         self.add_event_handler("session_start", self.session_start)
         self.add_event_handler("message", self.message)
 
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     socket.setdefaulttimeout(10)
     char_id = sys.argv[1]
     xmpp = Bot(char_id)
-    xmpp.connect(("v587.info",5222))
+    xmpp.connect(("bandwagon.v587.info",5222))
     xmpp.process(block=False)
     while (True):
         line = sys.stdin.readline()
@@ -68,7 +68,7 @@ if __name__ == '__main__':
             status = m.group(1)
             xmpp.send_presence(pstatus=status, pshow='xa')
         else:
-            xmpp.send_message(mto="messenger@v587.info/xkx", mbody=line, mtype='chat')
+            xmpp.send_message(mto="messenger@localhost/xkx", mbody=line, mtype='chat')
             m = re.match("http://pkuxkx.net/antirobot/robot.php", line)
             if m:
                 Thread(target=send_image_async,args=[xmpp,line,char_id]).start()
